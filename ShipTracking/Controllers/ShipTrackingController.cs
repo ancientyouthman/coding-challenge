6c4a3532e9d0ebc0bab678745bde94a95cb4e7c8
Joel <Joel@KEITH>
2020-01-16 22:56:22 +0000

    starting on the control panel

--- ShipTracking/Controllers/ShipTrackingController.cs
+++ ShipTracking/Controllers/ShipTrackingController.cs
@@ -1,4 +1,5 @@
-﻿using ShipTracking.Services;
+﻿using ShipTracking.Models;
+using ShipTracking.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,11 @@ using System.Web.Mvc;
 
 namespace ShipTracking.Controllers
 {
-    public class ComponentsController : Controller
+    public class ShipTrackingController : Controller
     {
         private readonly ShipTrackingService _shipTrackingService;
 
-        public ComponentsController()
+        public ShipTrackingController()
         {
             _shipTrackingService = new ShipTrackingService();
         }
@@ -21,6 +22,15 @@ namespace ShipTracking.Controllers
         {
             var grid = _shipTrackingService.GetGrid();
             return PartialView("_Grid", grid);
+        }
+
+        [HttpGet]
+        public ActionResult RenderControlPanel()
+        {
+            var ships = new List<ShipModel>();
+            var grid = _shipTrackingService.GetGrid();
+            if (grid != null && grid.Ships != null) ships = grid.Ships.ToList();
+            return PartialView("_ControlPanel", ships);
         }
     }
 }
