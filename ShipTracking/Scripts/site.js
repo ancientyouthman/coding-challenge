a260e1ee4de62c4357dff64e32f3fe57ce8fd486
Joel <Joel@KEITH>
2020-01-19 19:23:50 +0000

    happy with moving ships

--- ShipTracking/Scripts/site.js
+++ ShipTracking/Scripts/site.js
@@ -5,12 +5,11 @@ $(function () {
         $target.show();
         $target.html();
         for (var i = 0; i < errors.length; i++) {
-            $target.append('<p>' + errors[i].ErrorMessage  + '</p>')
+            $target.append('<p>' + errors[i].ErrorMessage + '</p>')
         }
     }
 
-    $('#btn-move-ships').on('click', function (e) {
-        e.preventDefault();
+    function MoveShips() {
         var $form = $('#form-move-ships');
         var $grid = $('#ship-tracking-grid');
         var $errorMessages = $('#move-ships-errors');
@@ -24,10 +23,28 @@ $(function () {
             success: function (response) {
                 $grid.removeClass('loading');
                 $grid.html(response);
+                UpdateControlPanel();
             },
             error: function (response) {
                 DisplayErrors(response.responseJSON.errors, $errorMessages);
-                }
+            }
         });
+    }
+
+    function UpdateControlPanel() {
+        var $controlPanel = $('#ships-control-panel');
+        $controlPanel.addClass('loading');
+        $.ajax({
+            type: 'GET',
+            url: '/ShipTracking/RenderControlPanel',
+            success: function (response) {
+                $controlPanel.removeClass('loading');
+                $controlPanel.html(response);
+            }
+        });
+    }
+
+    $(document).on('click', '#btn-move-ships', function () {
+        MoveShips();
     });
 });
